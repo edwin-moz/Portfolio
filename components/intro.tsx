0ba082bfe4036482147611f76bc0ab414d0fbb81
Edwin Moz <edwin.moz@icloud.com>
2024-01-07 01:48:17 -0600

    about

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,7 +9,8 @@ import { FaGithubSquare } from "react-icons/fa"
 
 export default function Intro() {
     return (
-        <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+        <section
+            className="mb-28 max-w-[50rem] text-center sm:mb-0">
             <div className="flex items-center justify-center">
                 <div className="relative">
                     <motion.div initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ type: "tween", duration: 0.2 }}>
@@ -20,13 +21,13 @@ export default function Intro() {
                     </motion.div>
                 </div>
             </div>
-            <motion.p className="mb-10 mt-4 px-4 text-2xl font-medium leading-[1.5] sm:text-4xl" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.1 }}>
-                <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
-                <span className="font-bold">full-stack developer</span> with{" "}
-                <span className="font-bold">8 years</span> of experience. I enjoy
+            <motion.h1 className="mb-10 mt-4 px-4 text-2xl font-medium leading-[1.5] sm:text-4xl" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ delay: 0.1 }}>
+                <span className="font-bold">Hello, I'm Edwin.</span> I'm a{" "}
+                <span className="font-bold">junior web developer</span> with{" "}
+                <span className="font-bold">less than 1 year</span> of experience. I enjoy
                 building <span className="italic">sites & apps</span>. My focus is{" "}
-                <span className="underline">React (Next.js)</span>.
-            </motion.p>
+                <span className="underline">React | (Next.js) | C# | .NET</span>.
+            </motion.h1>
             <motion.div className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }}>
 
                 <Link href="#contact" className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition">

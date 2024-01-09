ef2294c9d09e4609571d4fe22f8cac6628edbbac
Edwin Moz <edwin.moz@icloud.com>
2024-01-09 01:03:59 -0600

    theme

--- components/contact.tsx
+++ components/contact.tsx
@@ -26,8 +26,8 @@ export default function Contact() {
                 once: true
             }}>
             <SectionHeading>Contact me</SectionHeading>
-            <p className="text-gray-700 -mt-6">Please contact me directly at{" "} <a className="underline" href="mailto:edwin.moz@outlook.com">edwin.moz@outlook.com</a>{" "}or through this form.</p>
-            <form className="mt-10 flex flex-col" action={async formData => {
+            <p className="text-gray-700 -mt-6 dark:text-white/80">Please contact me directly at{" "} <a className="underline" href="mailto:edwin.moz@outlook.com">edwin.moz@outlook.com</a>{" "}or through this form.</p>
+            <form className="mt-10 flex flex-col dark:text-black" action={async formData => {
                 const { data, error } = await sendEmail(formData)
                 if (error) {
                     toast.error(error)
@@ -35,8 +35,8 @@ export default function Contact() {
                 }
                 toast.success("Email sent successfully")
             }}>
-                <input className="h-14 px-4 rounded-lg borderBlack" name="senderEmail" placeholder="Your email" type="email" required maxLength={500} />
-                <textarea className="h-52 my-3 rounded-lg borderBlack p-4" name="message" placeholder="Your message" required maxLength={5000} />
+                <input className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none" name="senderEmail" placeholder="Your email" type="email" required maxLength={500} />
+                <textarea className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none" name="message" placeholder="Your message" required maxLength={5000} />
                 <SubmitBtn />
             </form>
         </motion.section>
